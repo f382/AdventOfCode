126b9d833f568de751dc65efa25cd57b79244627
Martin Franke <Martin.Franke@cas.de>
2022-12-02 20:23:04 +0100

    Add solution for 2022-12-02

---
+++ 2022/aoc02_0.py
@@ -0,0 +1,26 @@
+"""Advent of Code 2022-12-02"""
+
+abc = ('A', 'B', 'C')
+xyz = ('X', 'Y', 'Z')
+pts = (1, 2, 3)
+rel = (1, 2, 0)
+
+with open("aoc02-1.txt", "r", encoding = "utf-8") as file:
+    lines = [l.strip() for l in file.readlines()]
+
+rounds_alpha = [l.split() for l in lines]
+
+rounds = [(abc.index(a), xyz.index(x)) for a, x in rounds_alpha]
+print(f"{rounds!r}")
+
+score = 0
+
+for r in rounds:
+    a, x = r
+    p = pts[x]
+    w = rel[x - a]
+    s = p + w * 3
+    print(f"{a} {x} -> {s}\n")
+    score += s
+
+print(f"{score}\n")
